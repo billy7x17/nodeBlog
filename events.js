a292d9d01c3013ced04e52f5764fdd9e33282fe2
billy119 <billy7x17@gmail.com>
2014-12-02 22:38:43 +0800

    测试下事件监听器是否对参数个数有要求。。。
    仔细想想，还真是白痴。
    
    js本来就对参数要求特别弱。。。

--- events.js
+++ events.js
@@ -1,7 +1,7 @@
 var events = require('events');
 var emitter = new events.EventEmitter();
-emitter.on('someEvent', function(arg1, arg2) {
-	console.log('listener1', arg1, arg2);
+emitter.on('someEvent', function(arg1, arg2 , arg3) {
+	console.log('listener1', arg1, arg2 , arg3);
 });
 emitter.on('someEvent', function(arg1, arg2) {
 	console.log('listener2', arg1, arg2);
