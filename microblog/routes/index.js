2d0497fd516128463f81852355f262263be5dc1f
billy119 <billy7x17@gmail.com>
2014-12-07 23:05:08 +0800

    路由测试

--- microblog/routes/index.js
+++ microblog/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hello' , function(req,res){
+	res.send('The time is ' + new Date().toString());
+});
+
 module.exports = router;
