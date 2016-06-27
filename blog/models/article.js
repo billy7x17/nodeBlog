5efb10c57e89cf88a8cbcef264d8ea5765a2cee3
billy119 <billy7x17@gmail.com>
2016-06-27 20:07:09 +0800

    文章save

--- blog/models/article.js
+++ blog/models/article.js
@@ -12,15 +12,21 @@ function Article(article) {
 }
 module.exports = Article;
 
-Article.prototype.save = function save (callback) {
+/**
+ * 存储文章
+ * @param callback 回调函数
+ */
+Article.prototype.save = function save(callback) {
 
+    /* 文章属性 */
     var article = {
-        title : this.title,
-        content : this.content,
-        lastUpdateTime : this.lastUpdateTime,
-        location : this.location
+        title: this.title,
+        content: this.content,
+        lastUpdateTime: this.lastUpdateTime,
+        location: this.location
     }
 
+    /* 数据库操作 */
     mongodb.open(function (err, db) {
         if (err) {
             return callback(err);
@@ -32,7 +38,45 @@ Article.prototype.save = function save (callback) {
                 return callback(err);
             }
 
-            collection.insert()
+            collection.insert(article, {safe: true}, function (err, article) {
+                mongodb.close();
+                callback(err, article);
+            })
         })
     })
+}
+
+Article.get = function get(title, callback) {
+
+    mongodb.open(function (err, db) {
+        if (err) {
+            callback(err);
+        }
+
+        db.collection('articles', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                callback(err);
+            }
+
+            var query = {};
+            if (title) {
+                query.title = title;
+            }
+
+            collection.find(query).sort({lastUpdateTime: -1}).toArray(function (err, articles) {
+                mongodb.close();
+                if (err) {
+                    callback(err, null);
+                } else {
+                    callback(null, articles);
+                }
+
+            })
+
+
+        })
+
+    })
+
 }
