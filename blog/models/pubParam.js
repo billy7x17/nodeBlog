6ea69bc4af52f007f13f06d6297fd8e2c2637684
billy7x17 <billy7x17@gmail.com>
2017-01-30 23:21:28 +0800

    签名算法

--- blog/models/pubParam.js
+++ blog/models/pubParam.js
@@ -2,6 +2,7 @@
  * Created by Administrator on 2017/1/26.
  */
 module.exports = pubParam;
+var encoding = require("encoding");
 var aliyunSettings = require('../aliyunSettings');
 function pubParam() {
 
@@ -9,14 +10,47 @@ function pubParam() {
 
 pubParam.generateSign = function (params) {
 
+    function percentEncode(originString) {
+
+        var result = new String("");
+
+        for (var char in originString) {
+            if (char in ['A', 'B', 'C']) {//TODO: all words
+                result += originString;
+            }
+            else {
+                result += '%' + originString.charCodeAt().toString(16);
+            }
+        }
+        return result;
+    }
+
     /* 配置文件赋值 */
     params.accessKeyId = aliyunSettings.accessKeyId;
     params.sms_signName = aliyunSettings.sms_signName;
     params.sms_templateCode = aliyunSettings.sms_templateCode;
 
-    var signString;
+    var signString = "POST&" + percentEncode("/") + "&";
+
+    var unsortedKeys = new Array;
 
     for (var key in params) {
-        signString += key + "%3D" + params.key;
+        console.log(key);
+        unsortedKeys.push(key);
     }
-}
+
+    console.log('keys:' + unsortedKeys);
+
+    var sortedKeys = unsortedKeys.sort();
+
+    console.log('sorted keys:' + sortedKeys);
+
+    for (var key in sortedKeys) {
+        signString += sortedKeys[key] + percentEncode("=") + percentEncode(params[sortedKeys[key]]);
+    }
+
+    return signString;
+};
+
+var testStr = pubParam.generateSign({'a': 'avalue', 'b': 'bvalue'});
+console.log(testStr);
