bb2be6fb19ecb087c69bf513e3893ede2c9e264d
billy119 <billy17x7@126.com>
2017-02-07 16:33:05 +0800

    阿里云各个参数的添加、更新

--- blog/aliyunSettings.js
+++ blog/aliyunSettings.js
@@ -6,6 +6,11 @@
  */
 module.exports = {
     accessKeyId: '3UpLTGggFSiYigKJ',
+    accessKeySecret: 'aH1mS9jG866ESl7PmfDC7uS2Q7fFVt',
     sms_signName: '博客通知',
-    sms_templateCode: 'SMS_44435024'
+    sms_templateCode: 'SMS_44435024',
+    sms_action: 'SingleSendSms',
+    version: '2016-09-27',
+    signatureVersion : '1.0',
+    signatureMethod : 'HMAC-SHA1'
 };
